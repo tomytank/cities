37a5c0a6cac90e1524ce9dcd4cd5713f15197e6f
tomytank <github@iamtom.com>
2020-04-04 13:52:52 +0200

    Installed proptypes and imported

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 import QuoteList from './components/QuoteList';
 
